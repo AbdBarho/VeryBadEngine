4333cbb922d39515591343913c7c094bc230fc7d
Abdullah Barhoum <ka70911@gmail.com>
2018-09-19 15:04:47 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,18 @@ module.exports = {
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime']
+        }
+      }
+    }]
   }
 };
